088af17a5d40d6e04b500ad6f88b8308082e13bd
acucar <acucar@cs.washington.edu>
2022-04-30 16:46:46 -0700

    added second scene for assignments, added buttons to switch scenes

--- src/main/java/org/studybuddy/App.java
+++ src/main/java/org/studybuddy/App.java
@@ -1,25 +1,60 @@
 package org.studybuddy;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  **/
-public class App extends Application {
-
+public class App extends Application implements EventHandler<ActionEvent> {
+    Stage window;
+    Scene timer, assignments;
+    Button goToAssignments;
+    Button goToTimer;
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        window = stage;
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 800);
-        stage.setScene(scene);
-        stage.show();
+        // scene 1, timer page
+        Label label1 = new Label("Study Timer");
+        goToAssignments = new Button("Assignments");
+        goToAssignments.setOnAction(this);
+
+        VBox timerLayout = new VBox();
+        timerLayout.getChildren().addAll(goToAssignments, label1);
+
+        timer = new Scene(timerLayout, 640, 800);
+
+
+
+        // scene 2, assignments
+        Label label2 = new Label("Assignment Tracker");
+        goToTimer = new Button("Study Timer");
+        goToTimer.setOnAction(this);
+
+        VBox assLayout = new VBox();
+        assLayout.getChildren().addAll(goToTimer, label2);
+
+        assignments = new Scene(assLayout, 640, 800);
+
+
+        window.setScene(timer);
+        window.show();
+    }
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == goToAssignments) {
+            window.setScene(assignments);
+        }
+        if (event.getSource() == goToTimer) {
+            window.setScene(timer);
+        }
     }
 
     public static void main(String[] args) {
