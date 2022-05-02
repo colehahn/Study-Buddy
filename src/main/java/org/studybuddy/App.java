0ff47eecf1c53a3c053a6331e52d4819602dbe5e
colehahn <colehahn2000@gmail.com>
2022-05-02 12:32:45 -0700

    reverted commit that broke things

--- src/main/java/org/studybuddy/App.java
+++ src/main/java/org/studybuddy/App.java
@@ -9,36 +9,42 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Timer;
-
 /**
  * JavaFX App
  **/
 public class App extends Application implements EventHandler<ActionEvent> {
-    Stage window;
+    Stage:: window;
     Scene timer, assignments;
     Button goToAssignments;
     Button goToTimer;
     @Override
     public void start(Stage stage) {
         window = stage;
-//        org.studybuddy.EventHandler handler = new org.studybuddy.EventHandler(
-//                window, goToAssignments, goToTimer, timer, assignments);
-
 
         // scene 1, timer page
-        timer = TimerScene.timerScene(new ActionEvent(), goToAssignments);
+        Label label1 = new Label("Study Timer");
+        goToAssignments = new Button("Assignments");
+        goToAssignments.setOnAction(this);
+
+        VBox timerLayout = new VBox();
+        timerLayout.getChildren().addAll(goToAssignments, label1);
+
+        timer = new Scene(timerLayout, 640, 800);
+
+
 
         // scene 2, assignments
         Label label2 = new Label("Assignment Tracker");
         goToTimer = new Button("Study Timer");
         goToTimer.setOnAction(this);
-        VBox assLayout = new VBox(50);
-        assLayout.getChildren().addAll(label2, goToTimer);
+
+        VBox assLayout = new VBox();
+        assLayout.getChildren().addAll(goToTimer, label2);
+
         assignments = new Scene(assLayout, 640, 800);
 
+
         window.setScene(timer);
-        window.setTitle("Study Buddy!");
         window.show();
     }
     @Override
