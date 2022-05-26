551bf4b9ba2e82b72fdd6c466da9f2fe87a8b396
colehahn <colehahn2000@gmail.com>
2022-05-25 21:44:43 -0700

    now adding blank buttons!

---
+++ src/main/java/org/studybuddy/MenuBarController.java
@@ -0,0 +1,39 @@
+package org.studybuddy;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MenuBarController {
+
+    @FXML
+    public Button timerButton;
+
+    @FXML
+    public Button assignmentsPageButton;
+
+    public void addAssignmentUI(ActionEvent actionEvent) throws Exception {
+        Scene scene = FXMLLoader.load(ClassLoader.getSystemClassLoader().getResource("Add Assignment UI.fxml"));
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToTimer() {
+         App.goToTimerScene();
+    }
+
+
+    @FXML
+    public void handleExit() {
+        //when the user leaves the app, we want to save their assignments
+    }
+}
